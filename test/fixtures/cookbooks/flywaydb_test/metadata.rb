a3ba5c96321177e73f316028d4f847cad02a1f06
Dennis Hoer <dennis.hoer@pearson.com>
2016-02-01 15:08:55 -0700

    Include recipe apt for ubuntu

--- test/fixtures/cookbooks/flywaydb_test/metadata.rb
+++ test/fixtures/cookbooks/flywaydb_test/metadata.rb
@@ -3,4 +3,5 @@ version '0.1.0'
 
 depends 'flywaydb'
 
+depends 'apt', '~> 2.0'
 depends 'mysql', '~> 6.0'
--- test/fixtures/cookbooks/flywaydb_test/recipes/mysql.rb
+++ test/fixtures/cookbooks/flywaydb_test/recipes/mysql.rb
@@ -1,3 +1,5 @@
+include_recipe 'apt' if platform?('ubuntu')
+
 mysql_service 'default' do
   version '5.6'
   bind_address '0.0.0.0'
