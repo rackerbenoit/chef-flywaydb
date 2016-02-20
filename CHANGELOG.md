320c876698c358e914765fc171d7b6978da42d08
Dennis Hoer <dennis.hoer@pearson.com>
2016-02-20 15:11:17 -0700

    Default sensitive to true

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## 3.0.0 2016-02-20
 
 - Remove global attributes ext_conf, conf, params, sensitive, and debug 
+- Default sensitive to true
 
 ## 2.0.0 2016-02-10
 
--- resources/default.rb
+++ resources/default.rb
@@ -6,7 +6,7 @@ attribute :ext_conf, kind_of: [String, Array, NilClass], default: nil
 attribute :conf, kind_of: [Hash, Array, NilClass], default: nil
 attribute :params, kind_of: Hash, default: {}
 attribute :sensitive, kind_of: [TrueClass, FalseClass], default: false
-attribute :debug, kind_of: [TrueClass, FalseClass], default: false
+attribute :debug, kind_of: [TrueClass, FalseClass], default: true
 attribute :install_dir, kind_of: String, default: lazy { node['flywaydb']['install_dir'] }
 attribute :user, kind_of: String, default: lazy { node['flywaydb']['user'] }
 attribute :group, kind_of: String, default: lazy { node['flywaydb']['group'] }
