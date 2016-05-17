1160e24158ef1b0d417ca941613b14a6cc501f8d
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-16 19:17:37 -0700

    Fix url issue with fedora

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,14 +5,6 @@ default['flywaydb']['user'] = 'flyway'
 default['flywaydb']['group'] = 'flyway'
 
 case node['platform_family']
-when 'debian', 'rhel'
-  default['flywaydb']['install_dir'] = '/opt'
-  default['flywaydb']['url'] = 'https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/' \
-    "#{node['flywaydb']['version']}/flyway-commandline-#{node['flywaydb']['version']}-linux-x64.tar.gz"
-  default['flywaydb']['sha256'] = 'f2cdc44f47dd0d10bb8fd7e34963982758454ffcb01216e314f8ac51c6decd08'
-  default['flywaydb']['mysql']['url'] = 'https://dev.mysql.com/get/Downloads/Connector-J/' \
-    "mysql-connector-java-#{node['flywaydb']['mysql']['version']}.tar.gz"
-  default['flywaydb']['mysql']['sha256'] = 'fa6232a0bcf67dc7d9acac9dc58910644e50790cbd8cc2f854e2c17f91b2c224'
 when 'windows'
   default['flywaydb']['install_dir'] = ENV['SYSTEMDRIVE']
   default['flywaydb']['url'] = 'https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/' \
@@ -21,4 +13,12 @@ when 'windows'
   default['flywaydb']['mysql']['url'] = 'https://dev.mysql.com/get/Downloads/Connector-J/' \
     "mysql-connector-java-#{node['flywaydb']['mysql']['version']}.zip"
   default['flywaydb']['mysql']['sha256'] = '6374dd729c96068b40cbdd9e42639b08f39c1b46260cfb4e58665e4b7d13322c'
+else
+  default['flywaydb']['install_dir'] = '/opt'
+  default['flywaydb']['url'] = 'https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/' \
+    "#{node['flywaydb']['version']}/flyway-commandline-#{node['flywaydb']['version']}-linux-x64.tar.gz"
+  default['flywaydb']['sha256'] = 'f2cdc44f47dd0d10bb8fd7e34963982758454ffcb01216e314f8ac51c6decd08'
+  default['flywaydb']['mysql']['url'] = 'https://dev.mysql.com/get/Downloads/Connector-J/' \
+    "mysql-connector-java-#{node['flywaydb']['mysql']['version']}.tar.gz"
+  default['flywaydb']['mysql']['sha256'] = 'fa6232a0bcf67dc7d9acac9dc58910644e50790cbd8cc2f854e2c17f91b2c224'
 end
