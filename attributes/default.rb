267dce0b4fb3b664b92d9d0bbb697c8821aa03d8
Kenneth Dsouza <kennethgds@gmail.com>
2020-06-30 14:53:02 -0500

    Update the http to https protocol
    Since http paths are failing, it's best to move the paths to https

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,13 +6,13 @@ default['flywaydb']['timeout'] = 259_200 # 72hrs
 # https://mariadb.com/kb/en/mariadb/about-mariadb-connector-j/
 default['flywaydb']['mariadb']['version'] = nil
 default['flywaydb']['mariadb']['sha256'] = nil
-default['flywaydb']['mariadb']['url'] = 'http://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/' \
+default['flywaydb']['mariadb']['url'] = 'https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/' \
   "#{node['flywaydb']['mariadb']['version']}/mariadb-java-client-#{node['flywaydb']['mariadb']['version']}.jar"
 
 # http://dev.mysql.com/doc/relnotes/connector-j/5.1/en/news-5-1.html
 default['flywaydb']['mysql']['version'] = '5.1.43'
 default['flywaydb']['mysql']['sha256'] = '81c8b2d94247d6db4c3ff2bacfe5c11f552cb6bc56c055b5625dec08d7f045c6'
-default['flywaydb']['mysql']['url'] = 'http://repo1.maven.org/maven2/mysql/mysql-connector-java/' \
+default['flywaydb']['mysql']['url'] = 'https://repo1.maven.org/maven2/mysql/mysql-connector-java/' \
   "#{node['flywaydb']['mysql']['version']}/mysql-connector-java-#{node['flywaydb']['mysql']['version']}.jar"
 
 # Generate SHA-256 from https://hash.online-convert.com/sha256-generator
